941e2bd9618346d62ad35d46553c35572040277d
TOnychen5632131 <162278003+TOnychen5632131@users.noreply.github.com>
2025-02-05 16:00:04 +0800

    更新合作列表

--- components/logos.tsx
+++ components/logos.tsx
@@ -23,14 +23,14 @@ export default function Logos() {
       <motion.div variants={itemVariants}>
         <TextBlur
           className="text-center text-2xl font-medium tracking-tight text-zinc-200 md:text-3xl"
-          text="Powered by"
+          text="Our Trusted Partners"
         />
       </motion.div>
 
       <motion.div variants={itemVariants}>
         <TextBlur
           className="text-center text-base text-zinc-300 sm:text-lg"
-          text="Simple and powerful tools that help you build faster"
+          text="Collaborating with industry leaders to drive innovation."
           duration={0.8}
         />
       </motion.div>
